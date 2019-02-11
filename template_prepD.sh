d9e3fda6233e85881d0aec2ffc7269e6345ba832
Golrokh <golrokh51@users.noreply.github.com>
2019-02-11 16:27:29 -0500

    Update template_prepD.sh

--- template_prepD.sh
+++ template_prepD.sh
@@ -3,7 +3,7 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-user=__EMAIL__
 #SBATCH --mail-type=__EMAIL_TYPE__
-#SBATCH --workdir=__WORKDIR__/__JOBID__/scripts/
+#SBATCH --workdir=__WORKDIR__/__JOBID__/scripts
 #SBATCH --output=__WORKDIR__/__JOBID__/results/_logs/prepD_slurm-%j.out
 #SBATCH --error=__WORKDIR__/__JOBID__/results/_logs/prepD_slurm-%j.err
 
