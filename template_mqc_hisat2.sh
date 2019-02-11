c954f2df7a3b3d65ca021daff7059ae3d2e8a5a6
Golrokh <golrokh51@users.noreply.github.com>
2019-02-11 16:27:04 -0500

    Update template_mqc_hisat2.sh

--- template_mqc_hisat2.sh
+++ template_mqc_hisat2.sh
@@ -2,7 +2,7 @@
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=__EMAIL__
 #SBATCH --mail-type=__EMAIL_TYPE__
-#SBATCH --workdir=__WORKDIR__/__JOBID__/scripts/
+#SBATCH --workdir=__WORKDIR__/__JOBID__/scripts
 #SBATCH --output=__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j.out
 #SBATCH --error=__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j.err
 
