9e3d7eb42097a5c2db8901ba44d510bff769f419
Golrokh <golrokh51@users.noreply.github.com>
2019-02-16 17:00:36 -0500

    Update template_mqc_hisat2.sh

--- template_mqc_hisat2.sh
+++ template_mqc_hisat2.sh
@@ -2,9 +2,9 @@
 #SBATCH --time=00:30:00
 #SBATCH --mail-user=__EMAIL__
 #SBATCH --mail-type=__EMAIL_TYPE__
-#SBATCH --workdir=__WORKDIR__/__JOBID__/scripts
-#SBATCH --output=__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j.out
-#SBATCH --error=__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j.err
+#SBATCH --chdir=$__WORKDIR__/__JOBID__/scripts
+#SBATCH --output=$__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j_%a.out
+#SBATCH --error=$__WORKDIR__/__JOBID__/results/_logs/mqc_hisat2_slurm-%j_%a.err
 
 export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
 module use $MUGQIC_INSTALL_HOME/modulefiles
