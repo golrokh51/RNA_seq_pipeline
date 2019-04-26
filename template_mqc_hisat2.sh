09a0deace96aebe74c193d538d9a143d9e3384f3
Golrokh <golrokh51@users.noreply.github.com>
2019-04-26 15:34:27 -0400

    Update template_mqc_hisat2.sh

--- template_mqc_hisat2.sh
+++ template_mqc_hisat2.sh
@@ -10,5 +10,5 @@ export MUGQIC_INSTALL_HOME=/cvmfs/soft.mugqic/CentOS6
 module use $MUGQIC_INSTALL_HOME/modulefiles
 module load mugqic/MultiQC/v1.6
 
-multiqc --outdir $__WORKDIR__/__JOBID__/results/_QC/ $__WORKDIR__/__JOBID__/results/_logs/*_summary.txt
+multiqc --outdir ../results/_QC/ .._/results/_logs/*_summary.txt
 
